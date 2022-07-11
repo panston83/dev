969be0972c47d5aae508d4990ad3a37b4430e0d0
panston83@gmail.com <panston83@gmail.com>
2022-07-12 00:27:05 +0900

    api test commit

---
+++ src/main/java/com/example/dev/api/RestApi.java
@@ -0,0 +1,55 @@
+package com.example.dev.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class RestApi {
+
+    @GetMapping("/getkobisData")
+    public String callAPI() {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        String jsonString = "";
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        HttpHeaders header = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(header);
+
+        String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
+
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"key=430156241533f1d058c603178cc3ca0e&targetDt=20120101").build();
+
+        ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
+        result.put("statusCode", resultMap.getStatusCodeValue());
+        result.put("header", resultMap.getHeaders());
+        result.put("body", resultMap.getBody());
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            jsonString = mapper.writeValueAsString(resultMap.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return jsonString;
+    }
+}
