9e5474481c0f7e10f30fcaa8e88cb797b9060d51
panston <panston83@gmail.com>
2022-08-05 18:18:04 +0900

    jpa and postgre setting

--- src/main/java/com/example/dev/api/BaseEntity.java
+++ src/main/java/com/example/dev/api/BaseEntity.java
@@ -1,5 +1,13 @@
 package com.example.dev.api;
 
-// 테이블로 생성되지 않도록 해주는 어노테이션
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass // 테이블로 생성되지 않도록 해주는 어노테이션
+@EntityListeners(value = {AuditingEntityListener.class}) // AuditingEntityListener : JPA 내부에서 엔티티 객체가 생성/변경 되는것을 감지하는 역할
+@Getter
 abstract class BaseEntity {
 }
