01004dace57c234d6b86cd471edc5f22019da09e
panston <panston83@gmail.com>
2022-08-19 18:24:29 +0900

    webClient start

---
+++ src/main/java/com/example/dev/controller/ApiGroup.java
@@ -0,0 +1,7 @@
+package com.example.dev.controller;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ApiGroup {
+    WebClient webClient = WebClient.create();
+}
